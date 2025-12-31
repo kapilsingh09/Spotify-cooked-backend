dfe4f8ba1b5262012f28f71801e828bc3f34910e
kapilsingh09 <karan404katyura@gmail.com>
2025-12-31 21:53:54 +0530

    refactor: Comment out hardcoded localhost origins in CORS configuration

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ dotenv.config({ path: './.env' });
 
 // BEST PRACTICE: SAFE ALLOWED ORIGINS
 const allowedOrigins = [
-  "http://localhost:5173",
-  "http://127.0.0.1:5173",
+  // "http://localhost:5173",
+  // "http://127.0.0.1:5173",
   process.env.FRONTEND_URL
 ];
 
